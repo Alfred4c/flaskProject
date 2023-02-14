04aed2401725e719deab89dca9fdd6539351f2a3
Alfred4c <109122219+Alfred4c@users.noreply.github.com>
2023-02-14 15:50:33 +0800

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -2,29 +2,32 @@ import pytest
 from app import *
 
 
-class Test_flask():
+class Test_flask:
     def setup(self):
         self.app = app
-        app.config['TESTING'] = True
+        app.config["TESTING"] = True
         self.client = self.app.test_client()
 
     def test_add(self):
         resp1 = self.client.post("/add", data={"text": "akjsdhaja"})
-        resp2 = self.client.post('/add', data={"text": "123456789123456789123456789123456789123456789123456789"})
+        resp2 = self.client.post(
+            "/add",
+            data={"text": "123456789123456789123456789123456789123456789123456789"},
+        )
 
         assert resp1.status_code == 302 and resp2.status_code == 404
 
     def test_finish(self):
-        resp1 = self.client.post('/finished', data={'id': 42})
+        resp1 = self.client.post("/finished", data={"id": 5})
         assert resp1.status_code == 302
 
     def test_delete(self):
-        resp = self.client.post('/delete', data={'id': 42})
+        resp = self.client.post("/delete", data={"id": 5})
         assert resp.status_code == 302
 
     def teardown(self):
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main("-s test_flask.py")
